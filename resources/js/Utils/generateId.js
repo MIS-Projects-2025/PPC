2c88e58d8d479f54904e1cc3c6064c0d0e92bafd
telfordmisph <telford_mis_ph@astigp.com>
2026-09-09 14:21:17 +0800

    fix: syntax

--- resources/js/Utils/generateId.js
+++ resources/js/Utils/generateId.js
@@ -1,11 +1,12 @@
-export default generateId = () => {
+// resources/js/Utils/generateId.js
+export default function generateId() {
   // Use native randomUUID if in secure context
   if (typeof crypto !== 'undefined' && typeof crypto.randomUUID === 'function') {
     return crypto.randomUUID();
   }
-  
-  // Safe fallback using crypto.getRandomValues (works over http/WSL/Docker)
+
+  // Safe fallback for HTTP / non-secure contexts
   return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
     (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4))).toString(16)
   );
-};
+}
